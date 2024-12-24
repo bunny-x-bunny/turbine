a2d6de4f41771b7de3fa9a3186e9a1ee231fccb9
bunny-x-bunny <kegare@protonmail.com>
2024-12-24 20:19:25 +0600

    page-projects

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('home'))->name('home');
 Route::get('/about', fn() => view('about'))->name('about');
+Route::get('/projects', fn() => view('projects'))->name('projects');
